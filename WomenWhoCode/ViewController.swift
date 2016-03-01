24b616cc3f5c639b8aae8f56e37ab03773b3f39e
Vnu <dearvnu@yahoo.in>
2016-02-29 20:05:06 -0800

    Initial Commit

---
+++ WomenWhoCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WomenWhoCode
+//
+//  Created by Vinu Charanya on 2/29/16.
+//  Copyright © 2016 WomenWhoCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
