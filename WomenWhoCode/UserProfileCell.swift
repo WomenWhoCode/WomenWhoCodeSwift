e7030c18a7ca092d9ecf36162931f1ba7e6d5b69
Prasanthi Relangi <prasanthi.relangi@gmail.com>
2016-03-08 23:48:07 -0800

    Add searchBar to the event listing page

--- WomenWhoCode/UserProfileCell.swift
+++ WomenWhoCode/UserProfileCell.swift
@@ -23,10 +23,10 @@ class UserProfileCell: UITableViewCell {
     
     var profile: Profile!{
         didSet{
-            nameLabel.text = profile.fullName!
-            networkLabel.text = profile.network.title!
+            nameLabel.text = profile.fullName ?? " "
+            networkLabel.text = profile.network.title! ?? " "
             //FixMe: Update the label name and some other columns of user to match Parse
-            awesomeCountLabel.text = "\(profile.followingCount!)"
+            awesomeCountLabel.text = "\(profile.awesomeCount ?? 0)"
             
 //            profileImage.setImageWithURL(NSURL(string: profile.imageUrl!)!)
         }
