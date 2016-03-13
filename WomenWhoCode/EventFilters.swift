d4a806a8b3f85428ae6f877a34806b705635f36c
Prasanthi Relangi <prasanthi.relangi@gmail.com>
2016-03-12 23:12:47 -0800

    Update EventFilter section
    Add EventFilter model
    Add EventFilters delegate

---
+++ WomenWhoCode/EventFilters.swift
@@ -0,0 +1,23 @@
+//
+//  EventFilters.swift
+//  WomenWhoCode
+//
+//  Created by Prasanthi Relangi on 3/12/16.
+//  Copyright © 2016 WomenWhoCode. All rights reserved.
+//
+
+import UIKit
+
+class EventFilters {
+    
+
+    var features: [String]?
+    var network: String?
+    
+    init() {
+        features = []
+        network = "Silicon Valley"
+    }
+    
+    
+}
