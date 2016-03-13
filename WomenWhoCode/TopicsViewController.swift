8ce0259c125623395ccf26f46522fd16fde2d600
mahagovind <Maha@Mahas-MacBook-Pro.local>
2016-03-13 16:44:42 -0700

    Topics Vie implementation #75

--- WomenWhoCode/TopicsViewController.swift
+++ WomenWhoCode/TopicsViewController.swift
@@ -13,6 +13,10 @@ class TopicsViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     var topics :[Feature] = []
     var subscribed_topics :[Feature] = []
+    var recommended_topics :[Feature] = []
+    var other_topics :[Feature] = []
+    var section_headers = ["Subscribed", "Recommended", "All Topics"]
+
     var subscription : [Subscribed] = []
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +26,6 @@ class TopicsViewController: UIViewController {
         tableView.estimatedRowHeight = 320
         tableView.rowHeight = UITableViewAutomaticDimension
         getTopics()
-        getSubscribed()
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +37,7 @@ class TopicsViewController: UIViewController {
     {
          ParseAPI.sharedInstance.getFeatures { (feature, error) -> () in
             self.topics = feature!
+            self.getSubscribed()
             self.tableView.reloadData()
         }
     }
@@ -50,6 +54,7 @@ class TopicsViewController: UIViewController {
     
     func setSubscribedTopics() {
         for(var i = 0 ; i < topics.count; i++) {
+            var added = false
             if ((topics[i].auto_subscribe != nil && topics[i].auto_subscribe!)) {
                 subscribed_topics.append(topics[i])
                 continue
@@ -57,10 +62,18 @@ class TopicsViewController: UIViewController {
             for (var j = 0 ; j < subscription.count; j++) {
                 if(subscription[j].feature_id == topics[i].objectId && subscription[j].subscribe != nil && subscription[j].subscribe!) {
                     subscribed_topics.append(topics[i])
+                    added = true
+                } else  if(subscription[j].feature_id == topics[i].objectId && subscription[j].recommended != nil && subscription[j].recommended!) {
+                    recommended_topics.append(topics[i])
+                    added = true
                 }
             }
+            if(!added) {
+                other_topics.append(topics[i])
+            }
         }
-        print("\(subscribed_topics.count)")
+        print("\(subscribed_topics.count)  \(recommended_topics.count)  \(other_topics.count)")
+        tableView.reloadData()
     }
 }
 
@@ -68,17 +81,54 @@ extension TopicsViewController: UITableViewDataSource, UITableViewDelegate{
         
         func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
             let cell = tableView.dequeueReusableCellWithIdentifier("TopicCell", forIndexPath: indexPath) as! TopicCell
-                    cell.feature = topics[indexPath.row]
+            if(indexPath.section == 0) {
+                cell.feature = subscribed_topics[indexPath.row]
+                cell.followImage.hidden = true
+                
+            } else if(indexPath.section == 1) {
+                cell.feature = recommended_topics[indexPath.row]
+                cell.followImage.hidden = false
+
+            } else {
+                cell.feature = other_topics[indexPath.row]
+                cell.followImage.hidden = false
+
+            }
+            
             return cell
         }
         
         func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
             tableView.deselectRowAtIndexPath(indexPath, animated: true)
         }
-        
-        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return topics.count
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        if(section == 0)
+        {
+           return subscribed_topics.count
+        } else if section == 1 {
+           return recommended_topics.count
         }
+        else {
+            return other_topics.count
+        }
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(tableView: UITableView,
+        titleForHeaderInSection section: Int) -> String? {
+            return section_headers[section]
+    }
+
+    
     }
    
 
