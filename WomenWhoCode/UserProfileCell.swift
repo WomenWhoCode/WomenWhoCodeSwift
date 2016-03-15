4cf7a9108f238276b3fcc314a4b44edc5d8d5515
Vnu <dearvnu@yahoo.in>
2016-03-14 22:43:20 -0700

    Clean up user profile cell #91

--- WomenWhoCode/UserProfileCell.swift
+++ WomenWhoCode/UserProfileCell.swift
@@ -22,80 +22,17 @@ class UserProfileCell: UITableViewCell {
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var networkLabel: UILabel!
-    @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var featuresLabel: UILabel!
-    @IBOutlet weak var awesomeImage: UIImageView!
-    @IBOutlet weak var awesomeCountLabel: UILabel!
-    
-    @IBOutlet weak var followersCount: UILabel!
-    
-    
-    @IBAction func onFollow(sender: AnyObject) {
-        
-        //Increment followersCount
-        print("Profile name: \(profile.fullName!) objectId: \(profile.objectId!)")
-        
-        let predicate = NSPredicate(format:"objectId == '\(profileId!)'")
-        let query = PFQuery(className: "Profile", predicate: predicate)
-        
-        //FIXME: prelangi : Refactor this code!!
-        query.findObjectsInBackgroundWithBlock {
-            (objects: [PFObject]?, error: NSError?) -> Void in
-            
-            if error == nil {
-                // The find succeeded.
-                print("Successfully retrieved \(objects!.count) profiles.")
-                
-                if let objects = objects {
-                    for object in objects {
-                        //self.profile.followersCount = self.profile.followersCount! + 1
-                        print("Updating followers count of \(self.profile.fullName!)")
-                        object["followers_count"] = self.profile.followersCount!+1
-                        object.saveInBackground()
-                        
-                        self.delegate?.userProfileCell(self, onFollow: true)
-                    }
-                }
-                
-            } else {
-                // Log details of the failure
-                print("Error: \(error!)")
-                
-                
-                
-            }
-        }
-        
-        
-        
-    }
-    
     
     var profile: Profile!{
         didSet{
             profileId = profile.objectId
             nameLabel.text = profile.fullName ?? " "
             networkLabel.text = profile.networkName! ?? " "
-            //            FixMe: Update the username and features label
-            
-            if let awesomeCount = profile.awesomeCount {
-                awesomeCountLabel.text = "\(profile.awesomeCount!)"
-            }
-            else {
-                awesomeCountLabel.text = "0"
-            }
             
             if(profile.imageUrl != nil) {
                 profileImage.setImageWithURL(NSURL(string: profile.imageUrl!)!)
             }
-            
-            if let followCount = profile.followersCount {
-                followersCount.text = " \(followCount) followers"
-            }
-            else {
-                followersCount.text = "No followers yet"
-            }
-            
         }
     }
     
